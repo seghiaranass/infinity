f3f99cf08df5fbccf604b2fe719b5c9acb269664
seghiaranass <anass.segh97@gmail.com>
2023-10-20 18:27:46 +0100

    Add width & max width to th row number #

---
+++ .history/multiple_modules_inherit/static/src/js/many2many_tags_field_20231020172419.js
@@ -0,0 +1,48 @@
+odoo.define('multiple_modules_inherit.many2many_tags_field',function(require){
+const {_t } = require("web.core")
+const { useService } = require("@web/core/utils/hooks");
+const { Many2ManyTagsFieldColorEditable } = require("@web/views/fields/many2many_tags/many2many_tags_field");
+const { patch } = require("@web/core/utils/patch");
+const Dialog = require('web.Dialog');
+
+patch(Many2ManyTagsFieldColorEditable.prototype, '/multiple_modules_inherit/static/src/js/many2many_tags_field.js',{
+    setup(){
+        this._super.apply(this,arguments);
+        this.action = useService("action");
+    },
+    onBadgeClick(ev, record) {
+        var copytext = ev.target.innerText;
+        var buttons = [
+            {
+                text: _t("Ok"),
+                classes: 'btn-primary',
+                close: true,
+            },
+        ];
+        new Dialog(self, {
+            size: 'large',
+            buttons: [
+                {
+                    text: _t("Open Form View"), classes: 'btn-primary', close: true,
+                    click: () => {
+                        this.action.doAction({
+                            type: 'ir.actions.act_window',
+                            res_model: this.props.relation,
+                            res_id: record.data.id,
+                            views: [[false, 'form']],
+                            target: 'current',
+                        });
+                    },
+                },
+                {
+                    text: _t("Cancel"), close: true
+                },
+            ],
+            $content: $('<div>', {
+                text: _t(" If you want to open a form of this click 'Open Form View'. "),
+            }),
+        }).open();
+        return this._super.apply(this, arguments);
+    }
+ })
+})
